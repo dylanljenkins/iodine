3d3724a0515afe4c16a94a9e17acbece5a0ed33a
dylanljenkins <dylanljenkins@gmail.com>
2022-10-07 23:25:29 +1100

    Extra input (#3)
    * Trying to add to IodineInput and failing.
    
    * Added more tests and more input types.
    
    * Cleaned up iml.

--- iodine_core/src/lib.rs
+++ iodine_core/src/lib.rs
@@ -1,25 +1,27 @@
 use derive_syn_parse::Parse;
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::LitStr;
+use syn::{LitStr, Token};
 
 #[derive(Parse)]
 pub struct IodineInput {
-    pub filename: LitStr,
+    filename: LitStr,
+    _comma_1: Token![,],
+    test: syn::Ident,
+    _comma_2: Token![,],
+    input_type: syn::Ident,
 }
 
 pub fn generate_test_tokens(input: IodineInput) -> TokenStream {
     let filename = input.filename;
+    let test = input.test;
+    let test_input_type = input.input_type;
 
     let output = quote! {
         #[test]
         pub fn test1() {
-            println!(#filename)
-        }
-
-        #[test]
-        pub fn test2() {
-            println!(#filename)
+            println!(#filename);
+            #test()
         }
     };
 
@@ -33,9 +35,25 @@ mod tests {
     use syn::LitStr;
 
     #[test]
-    pub fn test_generation() {
+    fn test_generation() {
+        pub struct TestInput {
+            to_print: String,
+        }
+
+        pub fn test(input: TestInput) {
+            println!("{}", input.to_print)
+        }
+
         let tokens = generate_test_tokens(IodineInput {
             filename: LitStr::new("test", Span::call_site()),
+            _comma_1: syn::token::Comma {
+                spans: [Span::call_site()],
+            },
+            test: syn::Ident::new("test", Span::call_site()),
+            _comma_2: syn::token::Comma {
+                spans: [Span::call_site()],
+            },
+            input_type: syn::Ident::new("TestInput", Span::call_site()),
         });
 
         println!("{tokens:?}")
--- tests/test.rs
+++ tests/test.rs
@@ -1,4 +1,17 @@
 #[cfg(test)]
-mod iodine_tests {
-    iodine::generate_tests!("tests22");
+mod tests {
+    struct Input {
+        number: usize,
+    }
+
+    fn test() {
+        println!("{}", 1)
+    }
+
+    #[test]
+    fn _dummy() {
+        assert_eq!(1, 1);
+    }
+
+    iodine::generate_tests!("filename", test, Input);
 }
